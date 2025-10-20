c0d5168727c351c39d2e4ad6d582d6059597963c
iftekhar moinuddin <134924046+moinnuddinnn@users.noreply.github.com>
2025-10-21 00:45:27 +0530

    Update detect_movement.py

--- detect_movement.py
+++ detect_movement.py
@@ -9,7 +9,6 @@ model = tf.keras.models.load_model('models/iris_model.h5')
 with open('models/label_encoder.pkl', 'rb') as f:
     label_encoder = pickle.load(f)
 
-# Initialize MediaPipe FaceMesh
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(
     max_num_faces=1,
@@ -69,5 +68,6 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
 
 
