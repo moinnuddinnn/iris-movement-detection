da27f0bcca3ff04413b14eaf3dce6d6fd182152f
iftekhar moinuddin <134924046+moinnuddinnn@users.noreply.github.com>
2025-10-20 20:56:14 +0530

    Update collect_data.py

--- collect_data.py
+++ collect_data.py
@@ -5,7 +5,6 @@ import csv
 import time
 import os
 
-# === CONFIG ===
 DATA_DIR = "data"
 OUT_FILE = os.path.join(DATA_DIR, "iris_dataset.csv")
 FRAMES_PER_LABEL = 30      # Number of frames to record per key press (~1 second at 30 FPS)
@@ -137,3 +136,4 @@ with mp_face.FaceMesh(
                         writer.writerow([timestamp, l_norm[0], l_norm[1], r_norm[0], r_norm[1], label])
                 time.sleep(DELAY_BETWEEN_FRAMES)
             print(f"[+] Done recording '{label}'!")
+
